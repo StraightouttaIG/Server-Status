975ed82a19f99971f086c81e8bdf190c877119bb
Nawaf AlKhaldi <H-2w@hotmail.com>
2020-10-05 14:20:39 +0300

    first commit

---
+++ Servers/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(345, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1800000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(12, 72)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(345, 229)
+        Me.TextBox1.TabIndex = 4
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 49)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(152, 18)
+        Me.CheckBox1.TabIndex = 5
+        Me.CheckBox1.Text = "hide online status"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(370, 314)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ Servers/Form1.vb
@@ -0,0 +1,134 @@
+﻿Imports System.ComponentModel
+Imports System.Net
+Imports Microsoft.VisualBasic.Devices
+Public Class Form1
+    Public gameslist As New List(Of String)
+    Dim att As Integer = 0
+    Dim str As New List(Of String)
+    Public wc As New Net.WebClient
+    Function getdescription(game As String)
+        Try
+            Dim str As String = wc.DownloadString("https://downdetector.com/status/" + game + "/")
+            Dim des As String = System.Text.RegularExpressions.Regex.Match(str, """description"": (.*?),").Groups.Item(1).Value
+            des = des.Replace("\u002D", "-")
+            des = des.Replace("""", Nothing)
+            Return des
+        Catch ex As Exception
+            If Not My.Computer.Network.IsAvailable Then
+                MsgBox("Lost connection!", MsgBoxStyle.Critical, "Error")
+            Else
+                MsgBox("Game not found!", MsgBoxStyle.Information, "Not found")
+            End If
+        End Try
+    End Function
+    Function Status(game As String) As String
+        Try
+            Dim wc As New WebClient
+            Dim s As String = wc.DownloadString("https://downdetector.com/status/" + game + "/")
+            If game = "gta5" Then
+                game = "GTA 5"
+            End If
+            game = game.Replace("-", " ")
+            If s.Contains("No problems at " + game) Then
+                Return "Online"
+            ElseIf s.Contains("Possible problems at " + game) Then
+                Return "possible problems"
+            Else
+                Return "Offline"
+            End If
+        Catch ex As Exception
+            If Not My.Computer.Network.IsAvailable Then
+                MsgBox("Lost connection!", MsgBoxStyle.Critical, "Error")
+            End If
+        End Try
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Games/apps list *case sensitive* according to "downdetector.com" standards '
+        gameslist.Add("Valorant")
+        gameslist.Add("Call-of-Duty")
+        gameslist.Add("Overwatch")
+        gameslist.Add("Fortnite")
+        gameslist.Add("Fifa")
+        gameslist.Add("gta5")
+        gameslist.Add("Instagram")
+        gameslist.Add("Snapchat")
+        gameslist.Add("Gmail")
+        gameslist.Add("Twitter")
+        gameslist.Add("Playstation-Network")
+        gameslist.Add("Xbox-Live")
+        gameslist.Add("Fall-Guys")
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ' Server-status project by NWA ' 
+        ' Instagram @_n8z ' 
+        Control.CheckForIllegalCrossThreadCalls = False
+        Timer1.Start()
+        Dim nwa = New Threading.Thread(AddressOf lloop) : nwa.Start()
+    End Sub
+    Sub lloop()
+        While True
+            Try
+                Parallel.ForEach(gameslist, Sub(game)
+                                                If Status(game).Contains("Online") Then
+                                                    'if the "OFFLINE" game or the game that has "PROBLEMS" is now online 
+                                                    'it will get removed from the offline/has problems list
+                                                    If str.Contains(game) Then
+                                                        str.Remove(game)
+                                                    End If
+                                                    If Not CheckBox1.Checked Then
+                                                        TextBox1.AppendText("*" + game + " : " + "ONLINE" + "*" + vbNewLine)
+                                                    End If
+                                                    att += 1
+                                                    Me.Text = "Online: " + att.ToString
+                                                ElseIf Status(game).Contains("possible problems") Then
+                                                    TextBox1.AppendText("*" + game + " : " + "HAS PROBLEMS" + "*" + vbNewLine)
+                                                    If Not str.Contains(game) Then
+                                                        str.Add(game)
+                                                        '   send status to discord server // discord(game, "Server Status: possible problems")
+                                                    End If
+                                                ElseIf Status(game).Contains("Offline") Then
+                                                    TextBox1.AppendText("*" + game + " : " + "OFFLINE" + "*" + vbNewLine)
+                                                    If Not str.Contains(game) Then
+                                                        str.Add(game)
+                                                        '   send status to discord server // discord(game, "Server Status: Offline")
+                                                    End If
+                                                End If
+                                            End Sub)
+                Threading.Thread.Sleep(TimeSpan.FromSeconds(2))
+                TextBox1.Clear()
+            Catch ex As Exception
+
+            End Try
+        End While
+    End Sub
+    Sub discord(game As String, status As String)
+        Try : Net.ServicePointManager.CheckCertificateRevocationList = False : Net.ServicePointManager.DefaultConnectionLimit = 300 : Net.ServicePointManager.UseNagleAlgorithm = False : Net.ServicePointManager.Expect100Continue = False : Net.ServicePointManager.SecurityProtocol = 3072
+            Dim Encoding As New Text.UTF8Encoding
+            Dim str As String = "{""embeds"":[{""title"":""" + game + """,""description"":""" + status + """,""color"":" + rndcolor() + "}]}"
+            Dim Bytes As Byte() = Encoding.GetBytes(str)
+            Dim AJ As Net.HttpWebRequest = DirectCast(Net.WebRequest.Create("*webhook*"), Net.HttpWebRequest)
+            With AJ
+                .Method = "POST"
+                .Proxy = Nothing
+                .ContentType = "application/json"
+                .ContentLength = Bytes.Length
+            End With
+            Dim Stream As IO.Stream = AJ.GetRequestStream() : Stream.Write(Bytes, 0, Bytes.Length) : Stream.Dispose() : Stream.Close()
+            Dim Reader As New IO.StreamReader(DirectCast(AJ.GetResponse(), Net.HttpWebResponse).GetResponseStream()) : Dim Text As String = Reader.ReadToEnd : Reader.Dispose() : Reader.Close()
+        Catch ex As WebException
+        End Try
+    End Sub
+    Function rndcolor() As String
+        Dim Random As New Random()
+        Dim color = String.Format("{0:X6}", Random.Next(0, 1000000))
+        Dim re = CInt("&H" & color)
+        Return re
+    End Function
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' clears the list every 10m to avoid spamming the messages
+        str.Clear()
+    End Sub
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        End
+    End Sub
+End Class
